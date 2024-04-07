72ebdddd90f1354788e5b99ae00b3eeac0b1a7d7
bobbyhadz <opmftw@gmail.com>
2024-04-07 14:25:05 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+// 👇️ import useState hook
+import {useState} from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = event => {
+    setCount(current => current + 1);
+  };
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  );
+};
+
+export default App;
